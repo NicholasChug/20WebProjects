ca965cf5362e539a5abf9c695a176690dd2439d6
Gabby and Nick Zagorski <nicholas.chug@gmail.com>
2020-03-19 11:28:59 -0400

    project 8 finished with all functionality working

--- mealFinder/script.js
+++ mealFinder/script.js
@@ -56,6 +56,21 @@ function getMealById(mealID) {
         })
 }
 
+// Fetch Random Meal from API
+function getRandomMeal() {
+    // Clears meals and heading
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        })
+}
+
 // Add Meal to DOM
 function addMealToDOM(meal) {
     const ingredients = [];
@@ -89,6 +104,7 @@ function addMealToDOM(meal) {
 
 // Event Listeners
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
