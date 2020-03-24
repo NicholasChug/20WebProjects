39bcf8cd475e1b9073feebd1ae25e0e831b34f66
Nicholas Chug <nicholas.chug@gmail.com>
2020-03-24 14:37:42 -0400

    project functionality finished

--- speechTextReader/script.js
+++ speechTextReader/script.js
@@ -1,55 +1,54 @@
 const main = document.querySelector('main');
 const voicesSelect = document.getElementById('voices');
-const textArea = document.getElementById('text');
-const readButton = document.getElementById('read');
-const toggleButton = document.getElementById('toggle');
-const closeButton = document.getElementById('close');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
 
-const data = [
-    {
+const data = [{
         image: './img/drink.jpg',
-        text: "I'm thirsty"
-    }, 
+        text: "I'm Thirsty"
+    },
     {
         image: './img/food.jpg',
         text: "I'm Hungry"
-    }, 
+    },
     {
         image: './img/tired.jpg',
         text: "I'm Tired"
-    }, 
+    },
     {
         image: './img/hurt.jpg',
         text: "I'm Hurt"
-    }, 
+    },
     {
         image: './img/happy.jpg',
         text: "I'm Happy"
-    }, 
+    },
     {
         image: './img/angry.jpg',
         text: "I'm Angry"
-    }, 
+    },
     {
         image: './img/sad.jpg',
         text: "I'm Sad"
-    }, 
+    },
     {
         image: './img/scared.jpg',
         text: "I'm Scared"
-    }, 
+    },
     {
         image: './img/outside.jpg',
         text: 'I Want To Go Outside'
-    }, 
+    },
     {
         image: './img/home.jpg',
         text: 'I Want To Go Home'
-    }, 
+    },
     {
         image: './img/school.jpg',
         text: 'I Want To Go To School'
-    }, 
+    },
     {
         image: './img/grandma.jpg',
         text: 'I Want To Go To Grandmas'
@@ -59,31 +58,45 @@ const data = [
 data.forEach(createBox);
 
 // Create speech boxes
-function createBox(item){
+function createBox(item) {
     const box = document.createElement('div');
 
-    const { image, text } = item;
+    const {
+        image,
+        text
+    } = item;
 
     box.classList.add('box');
+
     box.innerHTML = `
-        <img src='${image}' alt='${text}' />
-        <p class="info">${text}</p>
-    `;
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+        // Add active effect
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+    });
 
     main.appendChild(box);
-
-    // TODO speak event
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
 
 // Store voices
 let voices = [];
 
-// Get voices
 function getVoices() {
     voices = speechSynthesis.getVoices();
 
     voices.forEach(voice => {
         const option = document.createElement('option');
+
         option.value = voice.name;
         option.innerText = `${voice.name} ${voice.lang}`;
 
@@ -91,18 +104,41 @@ function getVoices() {
     });
 }
 
-// Voice has changed
+// Set text
+function setTextMessage(text) {
+    message.text = text;
+}
+
+// Speak text
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+// Set voice
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+// Voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices);
 
-// Toggle textbox 
-toggleButton.addEventListener('click', () => {
-    document.getElementById('text-box').classList.toggle('show');
-})
+// Toggle text box
+toggleBtn.addEventListener('click', () =>
+    document.getElementById('text-box').classList.toggle('show')
+);
 
-// close button
-closeButton.addEventListener('click', () => {
-    document.getElementById('text-box').classList.remove('show');
-})
+// Close button
+closeBtn.addEventListener('click', () =>
+    document.getElementById('text-box').classList.remove('show')
+);
 
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
+
+// Read text button
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value);
+    speakText();
+});
 
 getVoices();
